5d1cea5532cf7b206c1618f4c4820d9c93c71046
Abhishek <abhishektiwari4843@gmail.com>
2025-09-14 00:29:36 +0530

    logo changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar: React.FC = () => {
             rel="noopener noreferrer"
           >
             <img
-              src="/images/allitgoods.jpg"
+              src="/images/allitgoods.jpeg"
               alt="IT Services Logo"
               className="h-16 w-auto object-contain transition-transform duration-300 hover:scale-105 cursor-pointer"
             />
